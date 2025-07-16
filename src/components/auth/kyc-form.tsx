76772a55004d883fdf04a11c8ef05444fdcf8fc3
Italo Elias <Italo16rj@gmail.com>
2025-07-16 19:03:19 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/auth/kyc-form.tsx
+++ src/components/auth/kyc-form.tsx
@@ -25,6 +25,7 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import {
   Popover,
