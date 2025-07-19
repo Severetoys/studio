b4d0bbe73f3ca0f81b37a058125021601eff496e
Italo Elias <Italo16rj@gmail.com>
2025-07-19 23:42:35 +0000

    dobre o tamanho da janela e do nome Italo Santos dentro

--- src/components/layout/main-header.tsx
+++ src/components/layout/main-header.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 const MainHeader = () => {
     return (
-        <div className="relative w-full h-[60vh] text-center flex items-center justify-center bg-black">
+        <div className="relative w-full h-[80vh] text-center flex items-center justify-center bg-black">
           <Image
             src="https://placehold.co/1920x1080.png"
             alt="Hero background"
@@ -15,7 +15,7 @@ const MainHeader = () => {
             data-ai-hint="male model"
           />
           <div className="relative border-4 border-primary p-4 shadow-neon-red-strong">
-            <h1 className="text-6xl font-serif text-white text-shadow-neon-red">Italo Santos</h1>
+            <h1 className="text-9xl font-serif text-white text-shadow-neon-red">Italo Santos</h1>
           </div>
         </div>
     );
