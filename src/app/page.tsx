d0d52d9e5492c50ddb064f3b889114bcdc264cc1
Italo Elias <Italo16rj@gmail.com>
2025-07-19 23:46:36 +0000

    aumente o tamanho o botão Face ID  40% a mais

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -65,9 +65,9 @@ export default function HomePageContent() {
     <div className="flex flex-col items-center">
       <div className="p-4 md:p-8 bg-background flex flex-col items-center gap-6 w-full max-w-sm text-center">
         <Button 
-            className="w-full h-14 bg-primary/90 hover:bg-primary text-primary-foreground text-xl font-semibold shadow-neon-red-light hover:shadow-neon-red-strong transition-all duration-300"
+            className="w-full h-20 bg-primary/90 hover:bg-primary text-primary-foreground text-3xl font-semibold shadow-neon-red-light hover:shadow-neon-red-strong transition-all duration-300"
             onClick={() => router.push('/auth')}>
-            <Fingerprint className="h-8 w-8 mr-4" />
+            <Fingerprint className="h-12 w-12 mr-4" />
             Face ID
         </Button>
         
@@ -104,6 +104,7 @@ export default function HomePageContent() {
     
 
     
+
 
 
 
