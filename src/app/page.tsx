55471bb514cf299d8a3eade39af7f34063baa718
Italo Elias <Italo16rj@gmail.com>
2025-07-28 05:38:14 +0000

    cuidado para nao alterar em nada apenas o botão google e apple que aumen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -118,9 +118,9 @@ export default function Home() {
                         Cadastre-se com Face ID
                     </Button>
 
-                    <div className="flex justify-center items-center w-full max-w-full mt-8">
+                    <div className="flex justify-center items-center w-full max-w-full mt-4">
                         <div className="flex-1 transition-transform hover:scale-105" style={{ flexBasis: '42.5%'}}>
-                           <Image src="https://firebasestorage.googleapis.com/v0/b/authkit-y9vjx.firebasestorage.app/o/WhatsApp%20Image%202025-07-26%20at%2002.02.58%20(1).jpeg?alt=media&token=00683b6b-59ac-483c-93f4-6c879ab9b86c" alt="Google Pay" width={338} height={135} className="object-contain" style={{ transform: 'scale(2.34)' }}/>
+                           <Image src="https://firebasestorage.googleapis.com/v0/b/authkit-y9vjx.firebasestorage.app/o/WhatsApp%20Image%202025-07-26%20at%2002.02.58%20(1).jpeg?alt=media&token=00683b6b-59ac-483c-93f4-6c879ab9b86c" alt="Google Pay" width={338} height={135} className="object-contain" style={{ transform: 'scale(2.7)' }}/>
                         </div>
                         <div className="flex-shrink-0 mx-4 flex flex-col items-center px-[15%]">
                             <button className="transition-transform hover:scale-105" onClick={() => setIsPixModalOpen(true)}>
@@ -130,7 +130,7 @@ export default function Home() {
                             <p className="text-[10px] text-muted-foreground whitespace-nowrap">APENAS BRASIL</p>
                         </div>
                         <div className="flex-1 transition-transform hover:scale-105" style={{ flexBasis: '42.5%'}}>
-                           <Image src="https://firebasestorage.googleapis.com/v0/b/authkit-y9vjx.firebasestorage.app/o/WhatsApp%20Image%202025-07-26%20at%2002.02.58.jpeg?alt=media&token=3a91ba87-6df8-41db-a3bd-64f720e7feb2" alt="Apple Pay" width={338} height={135} className="object-contain" style={{ transform: 'scale(2.34)' }}/>
+                           <Image src="https://firebasestorage.googleapis.com/v0/b/authkit-y9vjx.firebasestorage.app/o/WhatsApp%20Image%202025-07-26%20at%2002.02.58.jpeg?alt=media&token=3a91ba87-6df8-41db-a3bd-64f720e7feb2" alt="Apple Pay" width={338} height={135} className="object-contain" style={{ transform: 'scale(2.7)' }}/>
                         </div>
                     </div>
 
