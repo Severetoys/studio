7ac3fc6aa4f5535f55377cfeb9044bbc5ca655bb
Italo Elias <Italo16rj@gmail.com>
2025-07-28 03:55:43 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.js
+++ next.config.js
@@ -1,6 +1,38 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'w7.pngwing.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'scontent.cdninstagram.com',
+      },
+       {
+        protocol: 'https',
+        hostname: 'scontent-gru2-2.cdninstagram.com',
+      },
+       {
+        protocol: 'https',
+        hostname: 'scontent-gru1-1.cdninstagram.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'pbs.twimg.com'
+      }
+    ],
+  },
 };
 
 module.exports = nextConfig;
