0d94c98007049fe322458cf38e6fcefa8259a32c
Italo Elias <Italo16rj@gmail.com>
2025-07-29 19:13:08 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" className="dark" suppressHydrationWarning>
       <head>
-          <Script id="google-tag-manager" strategy="afterInteractive">
+      </head>
+      <body className={`font-sans ${inter.variable} antialiased bg-background`}>
+        <Script id="google-tag-manager" strategy="afterInteractive">
             {`
               (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -35,9 +37,7 @@ export default function RootLayout({
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
               })(window,document,'script','dataLayer','GTM-XXXXXXX');
             `}
-          </Script>
-      </head>
-      <body className={`font-sans ${inter.variable} antialiased bg-background`}>
+        </Script>
         <noscript>
             <iframe
                 src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXXX"
