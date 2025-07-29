860e33362afe74113cee36ed5d4dc54e9f0ecb05
Italo Elias <Italo16rj@gmail.com>
2025-07-29 07:03:49 +0000

    https://api.cloudflare.com/client/v4/zones/f7173e3d8f93e1e2e37df05f0ffdb

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,9 +27,25 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" className="dark" suppressHydrationWarning>
       <head>
-          
+          <Script id="google-tag-manager" strategy="afterInteractive">
+            {`
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-XXXXXXX');
+            `}
+          </Script>
       </head>
       <body className={`font-sans ${inter.variable} antialiased bg-background`}>
+        <noscript>
+            <iframe
+                src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXXX"
+                height="0"
+                width="0"
+                style={{ display: "none", visibility: "hidden" }}
+            ></iframe>
+        </noscript>
         
         <div id="fb-root"></div>
         <Script id="facebook-jssdk" strategy="afterInteractive">
